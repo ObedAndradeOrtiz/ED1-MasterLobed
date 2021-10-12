a5ae20527098e907fc717706467a5a20328caf83
LobedGaming <86919211+LobedGaming@users.noreply.github.com>
2021-10-12 08:52:03 -0400

    Add files via upload

---
+++ Tareas-2-2021/Actividad-4/CSMemoriaGTI.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+typedef int direccion_mem;
+
+const int max_memoria = 20, nulo = -1;
+
+struct NodoM {
+	int dato;
+	string id;
+	direccion_mem link;
+};
+
+class CSMemoriaGTI {
+private:
+	NodoM mem[max_memoria]; //vector estatico con memoria ya creada
+	direccion_mem libre;
+
+public:
+	CSMemoriaGTI();
+	direccion_mem New_Espacio(string cad);
+	void Delete_Espacio(direccion_mem dir);
+	void Poner_dato(direccion_mem dir, string id, int valor);
+	int  obtener_dato(direccion_mem dir, string id);
+	int  Espacio_Disponible();
+	int  Espacio_ocupado();
+	bool dir_libre(direccion_mem dir);
+	void mostrar_memoria();
+};
